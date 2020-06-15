d2f6a121b1c0ea7f7a08d3962dea3b4d99894451
MichaelRefvem <mkrefvem@gmail.com>
2020-06-15 15:06:49 -0700

    initial server setup

--- server.js
+++ server.js
@@ -11,10 +11,16 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Application Middleware
-app.use(express.urlencoded({extended: true})); // Body Parser
+app.use(express.urlencoded({ extended: true })); // Body Parser
 app.use(express.static('public')); // Serve files from 'Public'
-app.use('view engine', 'ejs'); // Look in 'view' for EJS
+app.set('view engine', 'ejs'); // Look in 'view' for EJS
+
+// ROUTES
+// HOME
+app.get('/hello', (request, response) => {
+  response.render('pages/index.ejs');
+});
 
 app.listen(PORT, () => {
   console.log(`book server, listening on ${PORT}`);
-})
+});
